ba6a2cad0e88781a54a8b33e72bdbe6a14dcef0c
arttiwa <104729811+arttiwa@users.noreply.github.com>
2023-11-20 09:33:55 +0700

    Update SimpleController.cs hashpass

--- test/Controllers/SimpleController.cs
+++ test/Controllers/SimpleController.cs
@@ -3,12 +3,15 @@ using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR.Protocol;
 using MySqlConnector;
-//using test.Models;
+using System.Security.Cryptography;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using Newtonsoft.Json;
+using test.models;
 
 
 namespace test.Controllers
 {
-
     [Route("api/[controller]")]
     [ApiController]
     public class SimpleController : ControllerBase
@@ -20,7 +23,35 @@ namespace test.Controllers
         {
             connection = new MySqlConnection(yourConnectionString);
         }
+        
+        static string ComputeSha256Hash(string? rawData)
+        {
+            if (rawData == null)
+            {
+                return "";
+            }
+            string salt = "should to random";
 
+            string saltData = rawData + "" +salt;
+            //create a Sha256                
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(saltData));
+
+                // Convert byte array to a string
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+                
+            }
+
+        }
+
+        
         // [HttpGet]
         // [Route("api/my-route")]
         // public ActionResult<IEnumerable<string>> GetTModels()
@@ -30,7 +61,7 @@ namespace test.Controllers
         // }
 
         [HttpGet]
-        [Route("get-All")]
+        [Route("get-all")]
         public ActionResult<dynamic> GetAllUsers()
         {
             string query = @"SELECT * FROM users";
@@ -38,8 +69,8 @@ namespace test.Controllers
             MySqlDataReader myReader;
             DataTable table = new DataTable();
             connection.Open();
- 
-            MySqlCommand myCommand = new MySqlCommand(query, connection); 
+
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
             myReader = myCommand.ExecuteReader();
             table.Load(myReader);
 
@@ -47,7 +78,8 @@ namespace test.Controllers
             connection.Close();
 
             List<object> allUser = new List<object>();
-            for (int i = 0; i < table.Rows.Count; i++) {
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
                 Dictionary<string, object> dict = new Dictionary<string, object>        //แปลงjson
                 {
                     { "id", table.Rows[i]["id"] },
@@ -69,12 +101,12 @@ namespace test.Controllers
         public ActionResult<dynamic> GetUserbyID(int id)
         {
             string query = @"SELECT * FROM users  WHERE id = @userID";
-            
+
             MySqlDataReader myReader;
             DataTable table = new DataTable();
             connection.Open();
 
-            MySqlCommand myCommand = new MySqlCommand(query, connection); 
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
             myCommand.Parameters.AddWithValue("@userID", id);
 
             myReader = myCommand.ExecuteReader();
@@ -82,7 +114,7 @@ namespace test.Controllers
 
             myReader.Close();
             connection.Close();
-            
+
             if (table.Rows.Count == 0)
             {
                 return BadRequest();
@@ -106,12 +138,12 @@ namespace test.Controllers
         public ActionResult<dynamic> GetUserbyUserName(string uname)
         {
             string query = @"SELECT * FROM users  WHERE username = @userName";
-            
+
             MySqlDataReader myReader;
             DataTable table = new DataTable();
             connection.Open();
 
-            MySqlCommand myCommand = new MySqlCommand(query, connection); 
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
             myCommand.Parameters.AddWithValue("@userName", uname);
 
             myReader = myCommand.ExecuteReader();
@@ -119,7 +151,7 @@ namespace test.Controllers
 
             myReader.Close();
             connection.Close();
-            
+
             if (table.Rows.Count == 0)
             {
                 return BadRequest();
@@ -138,30 +170,104 @@ namespace test.Controllers
 
         }
 
-        // [HttpPost("api/my-route")]      
-        // public ActionResult<string> PostTModel(TModel model)
-        // {
-        //     return null;
-        // }
 
-        // [HttpPut("{id}")]
-        // public IActionResult PutTModel(int id, TModel model)
-        // {
-        //     return NoContent();
-        // }
 
-        // [HttpDelete("{id}")]
-        // public ActionResult<TModel> DeleteTModelById(int id)
-        // {
-        //     return null;
-        // }
-    }
+        [HttpGet]
+        [Route("getUser-NP")]
+        public ActionResult<dynamic> GetUserNameAndPassword(string uname, string pass)
+        {
+            string query = @"SELECT * FROM users  WHERE username = @uname AND password = @pass";
 
-    public class TModel
-    {
-        public int id { get; set;}
+            string hashedPass = ComputeSha256Hash(pass);
+            
+            MySqlDataReader myReader;
+            DataTable table = new DataTable();
+            connection.Open();
 
-        public string uname { get; set;}
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
+            myCommand.Parameters.AddWithValue("@uname", uname);
+            myCommand.Parameters.AddWithValue("@pass", hashedPass);
+
+            myReader = myCommand.ExecuteReader();
+            table.Load(myReader);
+
+            myReader.Close();
+            connection.Close();
+
+
+
+            if (table.Rows.Count == 0)
+            {
+                return BadRequest();
+            }
+
+
+            Dictionary<string, object> dict = new Dictionary<string, object>
+            {
+                { "id", table.Rows[0]["id"] },
+                { "username", table.Rows[0]["username"] },
+                { "password", table.Rows[0]["password"] },
+                { "admin", table.Rows[0]["admin"] },
+                { "tel", table.Rows[0]["tel"] },
+                { "email", table.Rows[0]["email"] },
+                { "name", table.Rows[0]["name"] }
+            };
+
+            return dict;
+            // Console.WriteLine("Raw data: {0}", pass);
+            // string? hashedPassw = ComputeSha256Hash(pass);
+            // Console.WriteLine("Hash {0}", hashedPassw);
+            // Console.WriteLine(ComputeSha256Hash(pass));
+            // //Console.ReadLine();
+
+            // Dictionary<string, object> dictss = dict; // Your existing dictionary
+
+            // var passwordObj = new { Password = dictss["password"] };
+
+            // string jsonString = JsonConvert.SerializeObject(passwordObj);
+
+            //return dict;
+            //return jsonString + "Hashed: " + hashedPassw; //System.Collections.Generic.Dictionary`2[System.String,System.Object]
+                                                          //8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92
+                                                          //return (dict,hashedPassw);;
+        }
+
+        [HttpPost]
+        [Route("PostUser-NP")]
+        public ActionResult<dynamic> CreateUser(User user)
+        {
+            string? pass = user.pass;
+            string hashedPass = ComputeSha256Hash(pass);
+
+
+            string query = @"INSERT INTO users(`username`, `password`,`admin`, `tel`, `email`,`name`)
+                        VALUES (@uname, @pass, @admin, @tel, @email, @name);";
+
+
+            DataTable table = new DataTable();
+            connection.Open();
+            MySqlCommand myCommand = new MySqlCommand(query, connection);
+
+            myCommand.Parameters.AddWithValue("@uname", user.username);
+            myCommand.Parameters.AddWithValue("@pass", hashedPass);
+            myCommand.Parameters.AddWithValue("@admin", user.admin);
+            myCommand.Parameters.AddWithValue("@tel", user.tel);
+            myCommand.Parameters.AddWithValue("@email", user.email);
+            myCommand.Parameters.AddWithValue("@name", user.name);
+
+            Console.WriteLine("Records Inserted Successfully");
+
+            int rowAff = myCommand.ExecuteNonQuery();
+            if (rowAff == 0)
+            {
+                return BadRequest("Error Generated");
+            }
+            connection.Close();
+            return Ok("Records Inserted Successfully");
+        }
+
+
+        
 
     }
 }
