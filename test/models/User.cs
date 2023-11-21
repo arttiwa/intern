c9535763cc9ffe603cd06c49aafa9626fcdc153b
arttiwa <104729811+arttiwa@users.noreply.github.com>
2023-11-21 17:07:03 +0700

    Add files via upload

---
+++ test/models/User.cs
@@ -0,0 +1,26 @@
+
+namespace test.models
+{
+    public class User
+    {
+        public int? id { get; set; }
+        public string? username { get; set; }
+        public string? pass { get; set; }
+        public bool admin { get; set; }
+        public string? name { get; set; }
+        public string? tel { get; set; }
+        public string? email { get; set; }
+
+    }
+}
+
+// {
+//    "id": 1,
+//    "username":  "asdfg",
+//    "password":  "zxcvb",
+//    "admin":  false,
+//    "tel":  "0987654321",
+//    "email":  "asdfghjkld@email.com",
+//    "name": "sawarudo"
+   
+// }
